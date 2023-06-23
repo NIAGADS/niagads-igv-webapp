877fb70c014457544f9cf48e0bbabbaca37877c9
fossilfriend <egreenfest@gmail.com>
2023-06-23 11:15:58 -0400

    flesh out BedXYFeature class; update parsing funcs

--- src/decoders/bedDecoder.tsx
+++ src/decoders/bedDecoder.tsx
@@ -1,10 +1,18 @@
 import igv from "igv/dist/igv.esm";
-import { BedXYFeatureType } from "@browser-types/features";
+// do not need the type; remove import, but leave in case we want it for rendering the tracks
+import { BedXYFeature as BedXYFeatureType } from "@browser-types/features";
+
+const EXPECTED_BED_FIELDS = ["chr", "start", "end", "score", "name", "strand", "cdStart", 
+    "cdEnd", "color", "blockCount", "blockSizes", "blockStarts"]
 
 export function decodeBedXY(tokens: any, header: any) {
-    if (tokens.length < 3) return undefined
 
-    const gffTags = header && header.gffTags
+    // Get X (number of standard BED fields) and Y (number of optional BED fields) out of format
+    let match = header.format.match(/bed(\\d{1,2})\\+(\\d+)/)
+    const X = parseInt(match[1])
+    const Y = parseInt(match[2])
+
+    if (tokens.length < 3) return undefined
 
     const chr = tokens[0]
     const start = parseInt(tokens[1])
@@ -12,112 +20,107 @@ export function decodeBedXY(tokens: any, header: any) {
     if (isNaN(start) || isNaN(end)) {
         return new igv.DecodeError(`Unparsable bed record.`)
     }
+
     // const feature = new igv.UCSCBedFeature({chr: chr, start: start, end: end, score: 1000})
-    let feature: BedXYFeatureType = {
-        chrom: chr,
-        chromStart: start,
-        chromEnd: end,
-        score: 1000
+    let feature = new BedXYFeature(chr,start,end);
+
+    // parse standard columns
+    if (X > 3) { // parse additional standard BED (beyond chr, start, end) columns
+        feature = parseStandardFeatures(feature, X, tokens)
     }
 
-    const columns = header.columnNames
-    //Get x and y out of format
-    let x = 0
-    let y = 0
-    const format = header.format
-    let match = format.match(/bed(\d{1,2})\+(\d{1,2})/)
-    if(match){
-        x = parseInt(match[1])
-        y = parseInt(match[2])
-    }
+    // parse optional columns
+    feature = parseOptionalFeatures(feature, tokens, X, header.columnNames)
 
-    //parse standard columns
-    feature = parseStandardFeatures(feature, x, tokens)
-    //parse optional columns
-    feature = parseOptionalFeatures(feature, tokens, x, columns)
-
-    // @ts-ignore
-    feature = new BedXYFeature(feature)
-
-    
     return feature
 }
 
-function parseStandardFeatures(feature: BedXYFeatureType, x: number, tokens: any): BedXYFeatureType {
-    let counter = 4;
+function parseBedToken(field: string, token: string) {
+    switch(field) {
+        case "name":
+            return token === "." ? '' : token
+        case "score":
+            if (token === ".") return 0
+            return Number(token)
+        case "strand":
+            return [".", "+", "-"].includes(token) ? token : null
+        case "cdStart":
+        case "cdEnd":
+            return parseInt(token)
+        case "color":
+            if (token === "." ||  token === "0") return null
+            return igv.IGVColor.createColorString(token)
+        default:
+            return token
+    }
+}
+
+function parseStandardFeatures(feature: BedXYFeature, X: number, tokens: any): BedXYFeature {
+    // loop with index from 3 to X - 1
+    // building an object { EXPECTED_FIELDS[index]: token[index]}
     try {
-
-        if (counter > x) return feature
-        if (!feature.name) {
-            feature.name = tokens[3] === '.' ? '' : tokens[3]
+        let attributes: any = {}
+        for (let index = 3; index < X; index++) {
+            let field:string = EXPECTED_BED_FIELDS[index]
+            let value = parseBedToken(field, tokens[index])
+            if (value === null) continue
+            if (typeof(value) === "number" && isNaN(value)) {
+                continue
+            }
+            attributes[field] = value
         }
-        counter++
 
-        if (counter > x) return feature
-        feature.score = tokens[4] === '.' ? 0 : Number(tokens[4])
-        if (isNaN(feature.score)) {
-            return feature
-        }
-        counter++
+        // add to the feature and return
 
-        if (counter > x) return feature
-        feature.strand = tokens[5]
-        if (!(feature.strand === '.' || feature.strand === '+' || feature.strand === '-')) {
-            return feature
-        }
-        counter++
-
-        if (counter > x) return feature
-        feature.cdStart = parseInt(tokens[6])
-        if (isNaN(feature.cdStart)) {
-            return feature
-        }
-        counter++
-
-        if (counter > x) return feature
-        feature.cdEnd = parseInt(tokens[7])
-        if (isNaN(feature.cdEnd)) {
-            return feature
-        }
-        counter++
-
-        if (counter > x) return feature
-        if (tokens[8] !== "." && tokens[8] !== "0")
-            feature.color = igv.IGVColor.createColorString(tokens[8])
-
-        return feature
     } catch (e) {
             console.error(e)
             return null
     }
 }
 
-function parseOptionalFeatures(feature: BedXYFeatureType, tokens: any, x:number, columns: any) {
+function parseOptionalFeatures(feature: BedXYFeature, tokens: any, X:number, columns: any): BedXYFeature {
     //go through tokens and add optional columns to feature
+
+    // some token parsing
+    // test if tokens is a number and if so, change it to a number?
+        // do we want to differentiate between floats & integers
+        // if a number is NaN? leave as is for now
+    // change all "." to null and store it    
+    // anything else you think of
     let optionalFeatures: any = {}
-    for(let i = x; i < columns.length; i++){
+    for(let i = X; i < columns.length; i++){
         optionalFeatures[columns[i]] = tokens[i]
     }
 
-    feature.info = optionalFeatures
+    feature.setAdditionalAttributes({"info":optionalFeatures})
     return feature
 }
 
-//@ts-ignore
-class BedXYFeature implements BedXYFeatureType{
 
-    constructor(properties: BedXYFeatureType) {
-        Object.assign(this, properties)
+class BedXYFeature {
+    chrom: string;
+    start: number;
+    end: number;
+    score: number;
+    info: any;
+
+    constructor(chr: string, start: number, end: number, score=1000) {
+        this.start = start;
+        this.end = end;
+        this.chrom = chr;
+        this.score = score;
+    }
+
+    setAdditionalAttributes(attributes: any) {
+        Object.assign(this, attributes)
     }
 
     getAttributeValue(attributeName: string): any {
-        if (this.hasOwnProperty(attributeName)) {
-            //@ts-ignore
-            return this[attributeName]
-            //@ts-ignore
-        } else if (this.info.hasOwnProperty(attributeName)) {
-            //@ts-ignore
-            return this.info[attributeName]
+        const key = attributeName as keyof BedXYFeature
+        if (this.hasOwnProperty(key)) {
+            return this[key]
+        } else if (this.info.hasOwnProperty(key)) {
+            return this.info[key]
         }
         else {
             return null
