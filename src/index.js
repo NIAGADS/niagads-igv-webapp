50281f37b84ac7fa627329cc99c4b1d8dbf9a34e
fossilfriend <egreenfest@gmail.com>
2024-12-05 09:46:12 -0500

    remove application

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App.tsx";
-
-const rootEl = document.getElementById("root");
-if (rootEl !== null) {
-  createRoot(rootEl).render(<App />);
-}
